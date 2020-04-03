07b4426242d76f168014cf2c4874b3a603e9d65e
Felipe Ferreira da Silva <dasifefe@gmail.com>
2020-04-03 08:18:25 -0300

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,14 @@ impl StringFixed {
             self.data[index] = 0;
         }
     }
+    pub fn from(&mut self, string: &str) {
+        let string_array_byte = string.as_bytes();
+        for index in 0..self.data.len() {
+            if index < string_array_byte.len() {
+                self.data[index] = string_array_byte[index];
+            } else {
+                self.data[index] = 0
+            }
+        }
+    }
 }
